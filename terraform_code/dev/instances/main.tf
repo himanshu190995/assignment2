eac4677eb8608afebf49b7562acfabacbd7a1183
himanshu190995 <115360581+himanshu190995@users.noreply.github.com>
2023-03-10 09:37:10 -0500

    Update main.tf

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -10,7 +10,7 @@ data "aws_ami" "latest_amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
   filter {
-    name   = "name"
+    name   = "name" 
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
